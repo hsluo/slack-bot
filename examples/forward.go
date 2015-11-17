d87b3fdc677995ed9e3c1b9ba033c6377e6d5ebf
hsluo <horseluo@gmail.com>
2015-11-18 00:18:01 +0800

    complete forward handler

--- examples/forward.go
+++ examples/forward.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -15,6 +16,8 @@ import (
 )
 
 const KEY = "fwdtable"
+
+var fwdtable map[string][]string
 
 func handleRegister(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
@@ -36,22 +39,17 @@ func handleRegister(w http.ResponseWriter, r *http.Request) {
 		if fromChan != channels[i].Name {
 			continue
 		}
-		fwdtable := make(map[string][]string)
-		if _, err := memcache.JSON.Get(ctx, KEY, &fwdtable); err == memcache.ErrCacheMiss {
-			if _, ok := fwdtable[channels[i].Id]; !ok {
-				fwdtable[channels[i].Id] = []string{toChan, query}
-				if err := memcache.JSON.Set(ctx, &memcache.Item{
-					Key:    KEY,
-					Object: fwdtable,
-				}); err != nil {
-					log.Errorf(ctx, "memcache set error: %v", err)
-				} else {
-					fmt.Fprintln(w, "added")
-				}
+
+		if fwdtable == nil {
+			fwdtable = make(map[string][]string)
+			_, err := memcache.JSON.Get(ctx, KEY, &fwdtable)
+			if err != nil && err != memcache.ErrCacheMiss {
+				log.Errorf(ctx, "memcache get error: %v", err)
+				return
 			}
-		} else if err != nil {
-			log.Errorf(ctx, "memcache get error: %v", err)
-		} else {
+		}
+
+		if _, ok := fwdtable[channels[i].Id]; !ok {
 			fwdtable[channels[i].Id] = []string{toChan, query}
 			if err := memcache.JSON.Set(ctx, &memcache.Item{
 				Key:    KEY,
@@ -59,30 +57,52 @@ func handleRegister(w http.ResponseWriter, r *http.Request) {
 			}); err != nil {
 				log.Errorf(ctx, "memcache set error: %v", err)
 			} else {
-				fmt.Fprintln(w, "updated")
+				fmt.Fprintf(w, "registered chan=%s query=%s", fromChan, query)
 			}
 		}
+
 		return
 	}
 }
 
+// forward is a cron job that fetches the latest messages from registered channels
+// and forwards them to other channels.
+// It should be implemented using RTM API, but it's not possible on GAE.
 func forward(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
-	//client := urlfetch.Client(ctx)
+	client := urlfetch.Client(ctx)
 
-	fwdtable := make(map[string][]string)
-	_, err := memcache.JSON.Get(ctx, KEY, &fwdtable)
-	if err == memcache.ErrCacheMiss {
-		return
-	} else if err != nil {
-		log.Errorf(ctx, "memcache get error: %v", err)
+	if fwdtable == nil {
+		fwdtable = make(map[string][]string)
+		_, err := memcache.JSON.Get(ctx, KEY, &fwdtable)
+		if err != nil {
+			if err != memcache.ErrCacheMiss {
+				log.Errorf(ctx, "memcache get error: %v", err)
+			}
+			return
+		}
 	}
 
 	for fromChan, v := range fwdtable {
-		oldest := time.Now().Add(-10 * time.Second).Unix()
-		messages, err := bot.ChannelsHistory(url.Values{
+		oldest := time.Now().Add(-15 * time.Second).Unix()
+		messages, err := bot.WithClient(client).ChannelsHistory(url.Values{
 			"channel": {fromChan},
 			"oldest":  {strconv.FormatInt(oldest, 10)},
 		})
+		if err != nil {
+			log.Errorf(ctx, "channels history error: %v", err)
+			continue
+		}
+		for i := range messages {
+			if matched, err := regexp.MatchString(v[1], messages[i].Text); matched {
+				bot.WithClient(client).ChatPostMessage(url.Values{
+					"channel": {v[0]},
+					"text":    {messages[i].Text},
+				})
+				log.Debugf(ctx, "%s", messages[i].Text)
+			} else if err != nil {
+				log.Errorf(ctx, "regexp error: %v", err)
+			}
+		}
 	}
 }
